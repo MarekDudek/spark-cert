2411b862fa56065b094ef6f87d1e7fc3f95fec7d
Marek Dudek <marek.dudek@gmail.com>
2016-01-28 19:54:55 +0100

    word-count tested

--- src/test/scala/interretis/sparkcert/rdd/WordCountSpec.scala
+++ src/test/scala/interretis/sparkcert/rdd/WordCountSpec.scala
@@ -1,7 +1,15 @@
 package interretis.sparkcert.rdd
 
-import org.scalatest.{Matchers, FlatSpec}
+import interretis.sparktesting.SeparateContext
+import org.scalatest.Matchers
 
-class WordCountSpec extends FlatSpec with Matchers {
+class WordCountSpec extends SeparateContext with Matchers {
 
+  "word-count" should "produce proper results" in { f =>
+    val lines = f.sc.parallelize(List("one", "one two", "one two three"))
+    val app = new WordCount
+    val result = app wordCount lines
+    val counts = result collectAsMap()
+    counts shouldBe Map("one" -> 3, "two" -> 2, "three" -> 1)
+  }
 }
