c1d70baabab614fb328cd9ac6384fe33e87c62d7
Marek Dudek <marek.dudek@gmail.com>
2016-01-31 11:34:28 +0100

    union example

--- src/test/scala/interretis/sparkcert/rdd/RandomExamplesSpec.scala
+++ src/test/scala/interretis/sparkcert/rdd/RandomExamplesSpec.scala
@@ -6,9 +6,24 @@ import org.scalatest.Matchers
 class RandomExamplesSpec extends SeparateContext with Matchers {
 
   "example with flatMap" should "work" in { f =>
+    // given
     val lines = f.sc.parallelize(Array("hello we are learning Spark from someplace", "We are also learning Hadoop from someplace"))
+    // when
     val flattened = lines flatMap (_ split " ")
     val firstWord = flattened first()
+    // then
     firstWord shouldBe "hello"
+  }
+
+  "example with union" should "work" in { f =>
+    // given
+    val words1 = Seq("An", "Apple", "a", "Day", "Keep", "Doctor", "Away")
+    val words2 = Seq("An", "Apple", "a", "Day")
+    val rdd1 = f.sc.parallelize(words1)
+    val rdd2 = f.sc.parallelize(words2)
+    // when
+    val union = rdd1 union rdd2
+    // then
+    union.collect() should contain theSameElementsAs words1 ++ words2
   }
 }
