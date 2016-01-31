e04962d8196e835633927321274019464b65a416
Marek Dudek <marek.dudek@gmail.com>
2016-01-31 16:47:04 +0100

    cogroup compared with outer join

--- src/test/scala/interretis/sparkcert/rdd/RandomExamplesSpec.scala
+++ src/test/scala/interretis/sparkcert/rdd/RandomExamplesSpec.scala
@@ -1,6 +1,7 @@
 package interretis.sparkcert.rdd
 
 import interretis.sparktesting.SeparateContext
+import org.apache.spark.rdd.RDD
 import org.scalatest.Matchers
 
 class RandomExamplesSpec extends SeparateContext with Matchers {
@@ -66,27 +67,60 @@ class RandomExamplesSpec extends SeparateContext with Matchers {
     occurences1.collect() should contain theSameElementsAs Seq(("1", 1), (" b", 1), ("c", 1), ("d", 1), ("2", 1), (" a", 1), ("b", 1), ("c", 1))
     occurences2.collect() should contain theSameElementsAs Seq(("1", 1), (" hadoopexam.com", 1), ("2", 1), (" quicktechie.com", 1))
     // when
-    val innerJoin = occurences1 join occurences2
+    val innerJoin: RDD[(String, (Int, Int))] = occurences1 join occurences2
     innerJoin.collect() should contain theSameElementsAs Seq(("1", (1, 1)), ("2", (1, 1)))
 
-    val outerJoin = occurences1 fullOuterJoin occurences2 cache()
+    val outerJoin: RDD[(String, (Option[Int], Option[Int]))] = occurences1 fullOuterJoin occurences2 cache()
     outerJoin.collect() should contain("b", (Some(1), None))
     outerJoin.collect() should contain(" hadoopexam.com", (None, Some(1)))
 
-    val leftJoin = occurences1 leftOuterJoin occurences2
+    val leftJoin: RDD[(String, (Int, Option[Int]))] = occurences1 leftOuterJoin occurences2
     val rightJoinRev = occurences2 rightOuterJoin occurences1
     val rightJoinRevSwapped = rightJoinRev.map { a => val (k, (l, r)) = a; (k, (r, l)) }
     leftJoin.collect() should contain theSameElementsAs rightJoinRevSwapped.collect()
 
-    val rightJoin = occurences1 rightOuterJoin occurences2
+    val rightJoin: RDD[(String, (Option[Int], Int))] = occurences1 rightOuterJoin occurences2
     val leftJoinRev = occurences2 leftOuterJoin occurences1
     val leftJoinRevSwapped = leftJoinRev.map { a => val (k, (l, r)) = a; (k, (r, l)) }
     rightJoin.collect() should contain theSameElementsAs leftJoinRevSwapped.collect()
   }
 
-  "groupByKey" should "be fed function of proper signature" in { f =>
+  "groupByKey" should "return RDD of proper signature" in { f =>
     val pairs = f.sc.parallelize(Seq((1, "a"), (2, "b"), (1, "c")))
-    val result = pairs.groupByKey()
+    val result: RDD[(Int, Iterable[String])] = pairs.groupByKey()
     result.collect() should contain theSameElementsAs Seq((1, Seq("a", "c")), (2, Seq("b")))
+  }
+
+  "reduceByKey" should "return RDD of proper signature" in { f =>
+    val pairs = f.sc.parallelize(Seq(("a", 1), ("b", 2), ("a", 3)))
+    val result: RDD[(String, Int)] = pairs.reduceByKey(_ + _)
+    result.collect() should contain theSameElementsAs Seq(("a", 4), ("b", 2))
+  }
+
+  "join with multiple occurances" should "work" in { f =>
+    // given
+    val rdd1 = f.sc.parallelize(Seq(('a', 1), ('b', 2), ('a', 3), ('c', 4)))
+    val rdd2 = f.sc.parallelize(Seq(('a', 5), ('b', 6), ('a', 7), ('d', 8)))
+    // when
+    val outerJoin = rdd1 fullOuterJoin rdd2
+    // then
+    outerJoin.collect() should contain theSameElementsAs Seq(
+      ('a', (Some(1), Some(5))),
+      ('a', (Some(1), Some(7))),
+      ('a', (Some(3), Some(5))),
+      ('a', (Some(3), Some(7))),
+      ('b', (Some(2), Some(6))),
+      ('c', (Some(4), None)),
+      ('d', (None, Some(8)))
+    )
+    // when
+    val grouped = rdd1 cogroup rdd2
+    // then
+    grouped.collect() should contain theSameElementsAs Seq(
+      ('a', (Seq(1, 3), Seq(5, 7))),
+      ('b', (Seq(2), Seq(6))),
+      ('c', (Seq(4), Nil)),
+      ('d', (Nil, Seq(8)))
+    )
   }
 }
