c3172f5ff20255001362a3dec50b21d4a0d27eb3
Marek Dudek <marek.dudek@gmail.com>
2016-02-07 14:58:48 +0100

    some assertions for test

--- src/test/scala/interretis/sparkcert/rdd/SqlExamples.scala
+++ src/test/scala/interretis/sparkcert/rdd/SqlExamples.scala
@@ -6,9 +6,9 @@ import org.scalatest.Matchers
 class SqlExamples extends SeparateSqlContext with Matchers {
 
   "reading JSON" should "work with HiveContext" in { f =>
-    val tweets = f.sql.jsonFile("src/main/resources/tweets.json")
+    val tweets = f.sql.read.json("src/main/resources/tweets.json")
     tweets.registerTempTable("tweets")
     val results = f.sql.sql("SELECT user.name, text FROM tweets")
-    results.collect().foreach(println)
+    results.count() shouldBe 2
   }
 }
