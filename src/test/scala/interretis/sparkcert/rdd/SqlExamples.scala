6b2b84fa1284120d577345a1130705be0d660614
Marek Dudek <marek.dudek@gmail.com>
2016-02-11 23:27:32 +0100

    joining with SQL

--- src/test/scala/interretis/sparkcert/rdd/SqlExamples.scala
+++ src/test/scala/interretis/sparkcert/rdd/SqlExamples.scala
@@ -1,6 +1,7 @@
 package interretis.sparkcert.rdd
 
 import interretis.sparktesting.SeparateSqlContext
+import org.apache.spark.sql.Row
 import org.scalatest.Matchers
 
 class SqlExamples extends SeparateSqlContext with Matchers {
@@ -22,6 +23,37 @@ class SqlExamples extends SeparateSqlContext with Matchers {
     val keys = records.map(row => s"${row(0)}")
     keys.collect() should have length 100
   }
+
+  "joining with SQL" should "work" in { f =>
+    import f.sqlCtx.implicits.rddToDataFrameHolder
+
+    // given
+    val i1 = Item("1", "first", 1, "c1")
+    val i2 = Item("2", "second", 2, "c2")
+    val i3 = Item("3", "third", 3, "c3")
+    val items = f.sc.parallelize(List(i1, i2, i3))
+    items.toDF().registerTempTable("items")
+
+    val c1 = Company("c1", "company-1", "city-1")
+    val c2 = Company("c2", "company-2", "city-2")
+    val companies = f.sc.parallelize(List(c1, c2))
+    companies.toDF().registerTempTable("companies")
+
+    // when
+    val joined = f.sqlCtx.sql("SELECT * FROM companies c JOIN items i on c.companyId = i.companyId")
+
+    // then
+    joined.collect() should contain theSameElementsAs Seq(
+      Row("c1", "company-1", "city-1", "1", "first", 1, "c1"),
+      Row("c2", "company-2", "city-2", "2", "second", 2, "c2")
+    )
+  }
 }
 
 case class Record(key: Int, value: String)
+
+
+//case class Item(id: String, name: String, unit: Int, companyId: String)
+
+//case class Company(companyId: String, name: String, city: String)
+
